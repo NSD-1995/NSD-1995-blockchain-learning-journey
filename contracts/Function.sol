060a7256fa1ba70bd76e771ca893f4275025669f
NSD-1995 <rajdivahar@gmail.com>
2025-12-15 22:45:11 +0530

    function update

--- contracts/Function.sol
+++ contracts/Function.sol
@@ -3,14 +3,35 @@ pragma solidity ^0.8.1;
 
 contract MyFunctionContract{
 
-    uint value=0;
+    // uint value=0;
 
-    function setter( uint x) public  {
-        value = x;
+    // function setter( uint x) public  {
+    //     value = x;
+    // }
+
+    // function getter() public  view returns(uint x) {
+    //     return value;
+    //     //
+    // }
+
+    //Struct and Array
+
+    struct People{
+
+        string Name;
+        uint age;
     }
 
-    function getter() public  view returns(uint x) {
-        return value;
-        //
+    People[] public  people;
+
+    function AddPeople(string memory _name, uint256 age) public  {
+        people.push(People({Name:_name ,age:age}));
+    }
+
+
+    function getArray(uint256 index) public view   returns(People memory) {
+
+        return people[index]; 
+
     }
 }
